c4d2a6c9cdbd032b1b4a90d7fefdae5f5001644c
ArunSivagnanam <Arun.s@live.dk>
2015-04-16 09:28:20 +0200

    first comit

---
+++ GoDaddyClient/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using GoDaddyClient.ServiceReference;
+
+namespace GoDaddyClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            ChatInterfaceClient client = new ChatInterfaceClient();
+
+            User u = new User();
+            u.name = "Arun";
+            Console.Write(client.Login(u));
+            Console.Read();
+        }
+    }
+}
