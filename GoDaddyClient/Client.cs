15d0fcf394b584fc75d6426e69c9071ec8634e92
ArunSivagnanam <Arun.s@live.dk>
2015-05-08 18:34:17 +0200

    latest

--- GoDaddyClient/Client.cs
+++ GoDaddyClient/Client.cs
@@ -17,7 +17,7 @@ namespace GoDaddyClient
 
         public User currentUser;
         public List<User> friendsList;
-        public List<User> friendsToAccept; // pending friends
+        public List<User> friendsToAccept; // pending friendsd
 
 
         public Client()
