b9b68fd66ce204d4979a7d2908ee87216014d24a
kristianj <kristianjacobsen1@hotmail.com>
2015-05-10 14:43:28 +0200

    friendlistevent

---
+++ GoDaddyClient/FriendListEvent.cs
@@ -0,0 +1,18 @@
+﻿using GoDaddyClient.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoDaddyClient
+{
+    public class FriendListEvent : EventArgs
+    {
+        public FriendListEvent(User u)
+        {
+            this.u = u;
+        }
+       public User u;
+    }
+}
